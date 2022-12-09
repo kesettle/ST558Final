dab78c067a832d56204a30aad7298d83e9e42c93
kesettle <kesettle@ncsu.edu>
2022-12-08 22:47:07 -0500

    update

--- ui.R
+++ ui.R
@@ -2,7 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(caret)
-##Insert amphibians dataset##
+##amphibians dataset
+amph <- read.csv2("amphibians.csv", skip = 1)
 
 dashboardPage(skin="green",
               #title
@@ -24,41 +25,98 @@ dashboardPage(skin="green",
                   tabItem(tabName = "about",
                           fluidRow(
                             #two columns for each of the two items
-                            column(6,
+                            column(3,
                                    #Purpose of the app
                                    h1("Purpose"),
                                    #box to contain description
                                    box(background="green",width=12,
-                                       h4("The purpose of this application is to explore and model data collected on amphibians in Poland.")
+                                       h4("The purpose of this application is to explore and model four different data sets depending on the user's choice.")
+                  #########################################################################
+                  ### May not do on user choice, but instead have separate tabs for each ##
+                  #########################################################################
                                    )
                             ),
-                            column(6,
+                            column(9,
                                    #Info on the data
                                    h1("About the Data"),
-                                   #box to contain description
-                                   box(background="green",width=12,
-                                       h4("The data is about ___. It's from ___. More info can be found at the following:")
-                                       )
+                                   fluidRow(
+                                     column(3,
+                                            box(background="green",width=12,
+                                                h2("Diamonds"),
+                                                h4("Found in the ", code("ggplot2"), " package, this data set includes prices and other attributes of almost 54,000 daimonds. More information about the data can be found ", strong(a(href="https://ggplot2.tidyverse.org/reference/diamonds.html", "here.", style = "color:pink")))
+                                            )
+                                       
+                                     ),
+                                     column(3,
+                                            box(background="green",width=12,
+                                                h2("Pottery"),
+                                                h4("Found in the ", code("HSAUR3"), " package, this data set includes chemical compositions of Romano-British pottery. More information about the data can be found ", strong(a(href="https://cran.r-project.org/web/packages/HSAUR3/HSAUR3.pdf", "here.", style = "color:pink")))
+                                            )
+                                     ),
+                                     column(3,
+                                            box(background="green",width=12,
+                                                h2("Kanga"),
+                                                h4("Found in the ", code("faraway"), " package, this data set includes skull measurements of different species of kangaroo. More information about the data can be found ", strong(a(href="https://www.rdocumentation.org/packages/faraway/versions/1.0.8/topics/kanga", "here.", style = "color:pink")))
+                                            )
+                                     ),
+                                     column(3,
+                                             box(background="green",width=12,
+                                                 h2("Batting"),
+                                                 h4("Found in the ", code("Lahman"), " package, this data set includes batting, pitching, and fielding statistics from 1871 to 2021. More information about the data can be found ", strong(a(href="https://www.seanlahman.com/baseball-archive/statistics/", "here.", style = "color:pink")))
+                                             )
+                                     )
                                    )
                             ),
                           fluidRow(tags$hr()),
                           #Image(s) related to data
                           fluidRow(
-                            column(6,
+                            column(3,
                                    #Purposes of each tab
-                                   h1("Layout of this App"),
+                                   h1("Layout"),
                                    #box to contain description
                                    box(background="green",width=12,
-                                       h4("The app has the following tabs, with the purpose of each tab listed beside it:")
-                                   )
-                            ),
-                            column(6,
-                                   #Purposes of each tab
-                                   align = "center",
-                                   img(src="amphibianCollage.jpg",
-                                       width = 600)
+                                       h4("The app has the following tabs, with the purpose of each tab listed beside it:",
+                                          tags$ul(
+                                            tags$li("About - Description of the app and data. You are here."),
+                                            tags$li("Data Exploration - Exploratory data analysis (EDA). Gives numerical and graphical summaries of chosen data."),
+                                            tags$li("Modeling - ",
+                                                    tags$ul(
+                                                      tags$li(),
+                                                      tags$li(),
+                                                      tags$li()
+                                                      )
+                                                    ),
+                                            tags$li()
+                                            )
+                                          )
+                                       )
+                                   ),
+                            column(9,
+                                   fluidRow(
+                                     column(3,
+                                            align = "center",
+                                            img(src="Rare-Green-Diamond-060ct-Natural-Loose-Fancy-Light-Green-Color-GIA-SI1-Oval-254572313852-1024x984.jpg",
+                                                width = 200)
+                                            ),
+                                     column(3,
+                                            align = "center",
+                                            img(src="5413866585_b3c8f36b53_b.jpg",
+                                                width = 200)
+                                            ),
+                                     column(3,
+                                            align = "center",
+                                            img(src="kangaroo-1670528960625-5264.jpg",
+                                                width = 200)
+                                            ),
+                                     column(3,
+                                            align = "center",
+                                            img(src="OSCRFZRKN5FJXFJCBXLEF574VM.jpg",
+                                                width = 200)
+                                            )
+                                     )
                                    )
                             )
+                  )
                   ),
                   
           ########EDIT HERE AND SERVER########
